ab2af80da36d375de9c4da71d6e9440882137c44
VinChezz <vin4auzer@gmail.com>
2025-08-02 13:46:33 +0300

    feat: add ConfigModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
