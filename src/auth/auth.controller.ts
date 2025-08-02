72e9219ce076622de78133edbf2d33b255f801e2
VinChezz <vin4auzer@gmail.com>
2025-08-02 13:46:06 +0300

    feat(auth): implement authentication via email, Google, and Facebook
    - Created AuthModule with JWT and OAuth strategies (Google, Facebook)
    - Implemented AuthService with methods for login, registration, token refresh, and OAuth handling
    - Added AuthController with the following routes:
      - POST /auth/login
      - POST /auth/register
      - POST /auth/login/access-token
      - POST /auth/logout
      - GET /auth/google and /auth/google/callback
      - GET /auth/facebook and /auth/facebook/callback
    - Handles setting and removing access and refresh tokens in cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,123 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
-  Delete,
+  Controller,
+  HttpCode,
+  UsePipes,
+  ValidationPipe,
+  Post,
+  Res,
+  Req,
+  Get,
+  UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { response, Response } from 'express';
+import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login')
+  async login(
+    @Body() dto: LoginDto,
+    @Res({ passthrough: true })
+    res: Response,
+  ) {
+    const { refreshToken, ...response } = await this.authService.login(dto);
+
+    this.authService.addRefreshTokenToResponse(res, refreshToken);
+
+    return response;
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login/access-token')
+  async GetNewTokens(
+    @Req() req: Request,
+    @Res({ passthrough: true })
+    res: Response,
+  ) {
+    const refreshTokenFromCookies =
+      req.cookies[this.authService.REFRESH_TOKEN_NAME];
+
+    if (!refreshTokenFromCookies) {
+      this.authService.removeRefreshTokenToResponse(res);
+      throw new UnauthorizedException('Refresh токен не пройшов');
+    }
+    const { refreshToken, ...response } = await this.authService.getNewTokens(
+      refreshTokenFromCookies,
+    );
+
+    this.authService.addRefreshTokenToResponse(res, refreshToken);
+
+    return response;
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('register')
+  async register(
+    @Body() dto: RegisterDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refreshToken, ...response } = await this.authService.register(dto);
+
+    this.authService.addRefreshTokenToResponse(res, refreshToken);
+
+    return response;
+  }
+
+  @HttpCode(200)
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    this.authService.removeRefreshTokenToResponse(res);
+    this.authService.removeAccessTokenToResponse(res);
+
+    return { message: 'Logout successful' };
+  }
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async google(@Req() _req) {}
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleCallback(@Req() req, @Res({ passthrough: true }) res: Response) {
+    const { refreshToken, accessToken, ...response } =
+      await this.authService.validateOAuthLogin(req);
+
+    console.log('Google auth response', { refreshToken, accessToken });
+
+    this.authService.addRefreshTokenToResponse(res, refreshToken);
+    this.authService.addAccessTokenToResponse(res, accessToken);
+
+    return { message: 'Google login successful', status: 200, ...response };
+  }
+
+  @Get('facebook')
+  @UseGuards(AuthGuard('facebook'))
+  async facebook(@Req() _req) {}
+
+  @Get('facebook/callback')
+  @UseGuards(AuthGuard('facebook'))
+  async facebookCallback(
+    @Req() req,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refreshToken, accessToken, ...response } =
+      await this.authService.validateOAuthLogin(req);
+
+    console.log('Facebook auth response', { refreshToken, accessToken });
+
+    this.authService.addRefreshTokenToResponse(res, refreshToken);
+    this.authService.addAccessTokenToResponse(res, accessToken);
+
+    return { message: 'Facebook login successful', status: 200, ...response };
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,155 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { Response } from 'express';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  EXPIRE_DAY_REFRESH_TOKEN = 1;
+  REFRESH_TOKEN_NAME = 'refreshToken';
+
+  EXPIRE_MINUTES_ACCESS_TOKEN = 15;
+  ACCESS_TOKEN_NAME = 'accessToken';
+
+  constructor(
+    private jwt: JwtService,
+    private userService: UserService,
+    private prisma: PrismaService,
+    private configService: ConfigService,
+  ) {}
+
+  async login(dto: LoginDto) {
+    const user = await this.validateUser(dto);
+    const tokens = this.issueTokens(user.id);
+
+    return { user, ...tokens };
+  }
+
+  async register(dto: RegisterDto) {
+    const oldUser = await this.userService.getByEmail(dto.email);
+
+    if (oldUser) throw new BadRequestException('User already exists');
+
+    const user = await this.userService.create(dto);
+    const tokens = this.issueTokens(user.id);
+
+    return { user, ...tokens };
+  }
+
+  async getNewTokens(refreshToken: string) {
+    const result = await this.jwt.verifyAsync(refreshToken);
+    if (!result) throw new UnauthorizedException('Error with refresh token');
+
+    const user = await this.userService.getById(result.id);
+
+    if (!user) throw new UnauthorizedException('User not found');
+
+    const tokens = this.issueTokens(user.id);
+
+    return { user, ...tokens };
+  }
+
+  private async validateUser(dto: LoginDto) {
+    const user = await this.userService.getByEmail(dto.email);
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
+
+  issueTokens(userId: string) {
+    const data = { id: userId };
+
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: '1h',
+    });
+
+    const refreshToken = this.jwt.sign(data, {
+      expiresIn: '7d',
+    });
+
+    return { accessToken, refreshToken };
+  }
+
+  async validateOAuthLogin(request: any) {
+    const { email, name, picture } = request.user;
+
+    if (!email) {
+      throw new BadRequestException('Email is required');
+    }
+    let user = await this.userService.getByEmail(email);
+
+    if (!user) {
+      user = await this.prisma.user.create({
+        data: {
+          email,
+          name: name || 'Not specified',
+          picture: picture || '/uploads/default-avatar.png',
+        },
+      });
+    }
+
+    if (!user) {
+      throw new Error('Failed to create user');
+    }
+    const tokens = this.issueTokens(user.id);
+
+    return { user, ...tokens };
+  }
+
+  addRefreshTokenToResponse(res: Response, refreshToken: string) {
+    const expiresIn = new Date();
+    expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN);
+
+    res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+      httpOnly: true,
+      domain: this.configService.get('SERVER_DOMAIN'),
+      expires: expiresIn,
+      secure: true,
+      sameSite: 'none',
+    });
+  }
+
+  addAccessTokenToResponse(res: Response, accessToken: string) {
+    const expiresIn = new Date();
+    expiresIn.setMinutes(
+      expiresIn.getMinutes() + this.EXPIRE_MINUTES_ACCESS_TOKEN,
+    );
+
+    res.cookie(this.ACCESS_TOKEN_NAME, accessToken, {
+      httpOnly: true,
+      domain: this.configService.get('SERVER_DOMAIN'),
+      expires: expiresIn,
+      secure: true,
+      sameSite: 'none',
+    });
+  }
+
+  removeRefreshTokenToResponse(res: Response) {
+    res.cookie(this.REFRESH_TOKEN_NAME, '', {
+      httpOnly: true,
+      domain: this.configService.get('SERVER_DOMAIN'),
+      expires: new Date(0),
+      secure: true,
+      sameSite: 'none',
+    });
+  }
+
+  removeAccessTokenToResponse(res: Response) {
+    res.cookie(this.ACCESS_TOKEN_NAME, '', {
+      httpOnly: true,
+      domain: this.configService.get('SERVER_DOMAIN'),
+      expires: new Date(0),
+      secure: true,
+      sameSite: 'none',
+    });
+  }
+}
