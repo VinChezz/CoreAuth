177f34e0b1699893d4b25468bd2d6df524c13db8
VinChezz <vin4auzer@gmail.com>
2025-08-05 23:45:37 +0300

    feat: created the email service and email controller with methods. - SendCode - async method which send the code with 6 digits in email and  code expires in 10 min. -VerifyCode  - async method which update isEmailVerified field and deleted the code.id during 10 min

---
+++ src/auth/controllers/email/email.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  EmailVerificationDto,
+  VerifyEmailDto,
+} from 'src/auth/dto/email-dto/email-verefication.dto';
+import { EmailService } from 'src/auth/services/email/email.service';
+
+@Controller('auth/email')
+export class EmailController {
+  constructor(private emailService: EmailService) {}
+
+  @Post('send-code')
+  @HttpCode(200)
+  sendCode(@Body() dto: EmailVerificationDto) {
+    return this.emailService.sendCode(dto);
+  }
+
+  @Post('verify-code')
+  @HttpCode(200)
+  verifyCode(@Body() dto: VerifyEmailDto) {
+    return this.emailService.verifyCode(dto);
+  }
+}
---
+++ src/auth/services/email/email.service.ts
@@ -0,0 +1,78 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { generate6DigitsCode } from 'src/utils/generate6DigitsCode';
+import { PrismaService } from 'src/prisma.service';
+import {
+  EmailVerificationDto,
+  VerifyEmailDto,
+} from 'src/auth/dto/email-dto/email-verefication.dto';
+import { EmailSettingsService } from './email-settings.service';
+@Injectable()
+export class EmailService {
+  constructor(
+    private prisma: PrismaService,
+    private emailSettings: EmailSettingsService,
+  ) {}
+
+  async sendCode(dto: EmailVerificationDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new BadRequestException('User not found');
+
+    const code = generate6DigitsCode();
+    const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
+
+    await this.prisma.emailVerificationToken.deleteMany({
+      where: { userId: user.id },
+    });
+
+    await this.prisma.emailVerificationToken.create({
+      data: { code, expiresAt, userId: user.id },
+    });
+
+    const subject = `Email confirmation code`;
+    const text =
+      `Your email verification code: ${code}\n` +
+      `Expires in ${expiresAt.toLocaleTimeString()}`;
+
+    await this.emailSettings.sendEmail(dto.email, subject, text);
+
+    return { message: 'Email verefication code sent to email' };
+  }
+
+  async verifyCode(dto: VerifyEmailDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new BadRequestException('User not found');
+
+    const token = await this.prisma.emailVerificationToken.findFirst({
+      where: {
+        user: {
+          email: dto.email,
+        },
+        code: dto.code,
+        expiresAt: {
+          gte: new Date(),
+        },
+      },
+    });
+    console.log({ token });
+    if (!token || token.expiresAt < new Date()) {
+      throw new BadRequestException('Incorrect or expired code');
+    }
+
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: { isEmailVerified: true },
+    });
+
+    await this.prisma.emailVerificationToken.delete({
+      where: { id: token.id },
+    });
+  }
+}
