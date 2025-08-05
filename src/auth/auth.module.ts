60f3e71e50df3abd94c1d05953c2a10a1b36e295
VinChezz <vin4auzer@gmail.com>
2025-08-05 23:47:49 +0300

    feat: in import added PrismaModule, in controllers EmailController and PasswordController, in exports added EmailSettingsService and in providers added EmailSettingsService, EmailService and PasswordService,

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,18 +8,34 @@ import { getJwtConfig } from 'src/config/getJwt.config';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { FacebookStrategy } from './strategies/facebook.strategy';
+import { EmailSettingsService } from './services/email/email-settings.service';
+import { PrismaModule } from 'src/prisma.module';
+import { EmailService } from './services/email/email.service';
+import { PasswordService } from './services/password/password.service';
+import { EmailController } from './controllers/email/email.controller';
+import { PasswordController } from './controllers/password/password.controller';
 
 @Module({
   imports: [
     UserModule,
     ConfigModule,
+    PrismaModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: getJwtConfig,
     }),
   ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, GoogleStrategy, FacebookStrategy],
+  controllers: [AuthController, EmailController, PasswordController],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    GoogleStrategy,
+    FacebookStrategy,
+    EmailSettingsService,
+    EmailService,
+    PasswordService,
+  ],
+  exports: [EmailSettingsService],
 })
 export class AuthModule {}
