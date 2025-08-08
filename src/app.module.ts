38a45f44143d8d97cc7890f60b8de4f431cfd6f8
VinChezz <vin4auzer@gmail.com>
2025-08-08 23:31:23 +0300

    feat: added QrLoginModule in imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma.module';
+import { QrLoginModule } from './qr-login/qr-login.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { PrismaModule } from './prisma.module';
     AuthModule,
     UserModule,
     PrismaModule,
+    QrLoginModule,
   ],
   controllers: [AppController],
 })
