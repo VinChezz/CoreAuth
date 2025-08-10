08a9f84a4ca58668c502469c61b91d9b5c0eb3d8
VinChezz <vin4auzer@gmail.com>
2025-08-10 22:16:33 +0300

    feat: added cookie pasrser

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  app.use(cookieParser());
 
   await app.listen(port);
   console.log(`🚀 Server is running on http://localhost:${port}`);
