1706da6d0c3a53082c2021a77efe6fc4244a8fd9
VinChezz <vin4auzer@gmail.com>
2025-08-12 18:23:59 +0300

    feat: connect to the project framework swagger

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CoreAuth API')
+    .setDescription('CoreAuth API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const port = process.env.PORT || 3000;
 
